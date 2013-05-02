6fad74cd1e281994980d359682f0400044d1023c
Everton Luiz Ferreira <tom.msn@hotmail.com>
2013-05-02 13:34:13 -0300

    new

---
+++ AbstractFactory/FabricaSuzuki.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbstractFactory
+{
+    class FabricaSuzuki : FabricaMontadora
+    {
+        public override Moto CriaMoto()
+        {
+            return new Katana();
+        }
+        public override Carro CriaCarro()
+        {
+            return new Vitara();
+        }
+    }
+}
+
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Cria a FabricaHonda com seus respectivos automóveis
+            FabricaHonda honda = new FabricaHonda();
+            Automoveis auto = new Automoveis(honda);
+            // Cria a FabricaSuzuki com seus respectivos automóveis
+            FabricaSuzuki suzuki = new FabricaSuzuki();
+            auto = new Automoveis(suzuki);
+        }
+
+    }
+}
